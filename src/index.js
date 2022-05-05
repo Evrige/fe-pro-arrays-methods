85d1cd0e3facb210fde21fcb62e063f0ec4bdbc2
Artur Dudnik <arthur.dudnik21@gmail.com>
2022-05-05 14:02:46 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,59 +1,59 @@
 import { goods } from './goods.js';
-
+const filter = (param, value) => goods.filter(good => good[param] === value);
 /**
  * @param {string} brand
  * @returns {*}
  */
-const brandFilter = (brand) => {};
+const brandFilter = (brand) => filter("brand", brand);
 
 /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => {};
+const colorFilter = (color) => filter("color", color);
 
 /**
  * @param {string} model
  * @returns {*}
  */
-const modelFilter = (model) => {};
+const modelFilter = (model) => filter("model", model);
 
 /**
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => {};
+const memoryFilter = (memory) => filter("memory", memory);
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) => {};
+const priceFilter = (price) => filter("price", price);
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => {};
+const countryFilter = (country) => filter("country", country);
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => {};
+const osFilter = (os) => filter("os", os);
 
 /**
  * @param {number} from
  * @param {number} to
  */
-const rangeFilter = (from, to) => {};
+const rangeFilter = (from, to) => goods.filter(good => good.price >= from && good.price <= to);
 
-const minPriceReducer = () => {};
+const minPriceReducer = () => goods.reduce((minPrice, good)=> good.price < minPrice ? good.price : minPrice, goods[0].price);
 
-const maxPriceReducer = () => {};
+const maxPriceReducer = () => goods.reduce((minPrice, good)=> good.price > minPrice ? good.price : minPrice, goods[0].price);
 
-const toMaxSorter = () => {};
-const toMinSorter = () => {};
+const toMaxSorter = () => goods.sort((a, b) => b.price - a.price);
+const toMinSorter = () => goods.sort((a, b) => a.price - b.price);
 
 export const filters = {
   brandFilter,
